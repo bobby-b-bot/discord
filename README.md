fbdb3e54c1683e7a07edfe9bb15912cb497583f1
Felipe Zanettini <fzan@protonmail.com>
2019-06-12 17:46:11 -0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,50 @@
-# discord
+# Bobby B Bot - Discord version
+
+In this repository you can find the Discord version of the Bobby B Bot.  
+
+## How to use it
+
+Simply mention the bot's name in your channel '@bobby-b-bot' after adding it via [this link](https://bit.ly/2C0kogN), and the bot will reply with a random quote.
+
+## How to install
+
+1. Create a virtual environment and activate it (this is optional but when working with Python, I cannot recommend it enough) or create a root folder that will hold all the code;
+2. Clone discord repository inside this virtual enviroment folder (let's call it 'root') and then clone [utils](https://github.com/bobby-b-bot/utils.git) repository. The final structure should be somewhat similar to this:
+
+```
++ root
+└───+ discord
+│     |-- discord_bot.py
+└───+ utils
+      |-- __init__.py
+      |-- core.py
+      |-- logging_config.ini
+      |-- quotes.json
+      |-- triggers.json
+```
+
+4. Run command `pip install -r requirements.txt` in discord directory (this should install the requirements for utils as well, otherwise, you can also run the command in utils folder);
+5. Done, you are ready to configure it.
+
+#### TL;DR Installation:
+
+```
+$ python -m venv <venv_name>
+$ cd venv_name
+$ bin/activate
+(venv_name) $ git clone https://github.com/bobby-b-bot/discord.git
+(venv_name) $ git clone https://github.com/bobby-b-bot/utils.git
+(venv_name) $ cd discord
+(venv_name) $ pip install -r requirements.txt
+```
+
+## How to configure and run
+
+1. Create and maintain the .env file for environment variables in root discord folder (ENV = 'TEST' or 'PROD' and Discord token in variable TOKEN) 
+1. Create and mantain a logging_config.ini file in utils folder for logging configuration ([see documentation](https://docs.python.org/3/library/logging.config.html#logging-config-fileformat));
+1. Run the bot (`python discord_bot.py`)
+1. Have fun!
+
+## How to contribute
+
+Feature requests such as new quotes are welcome via issues on GitHub! Feel free to contribute. You can also contribute by donating via [PayPal](http://paypal.me/felipezanettini) to keep the servers running. 
---
+++ discord_bot.py
@@ -0,0 +1,72 @@
+# Standard library imports
+import os
+import logging
+from logging.config import fileConfig
+
+# Third party imports
+import discord
+
+# Local application imports
+from utils.core import get_env, get_random_quote # bot standard functions
+
+# validate all mandatory files exist before starting
+assert os.path.isfile('../utils/logging_config.ini') # Logs config file
+assert os.path.isfile('.env')                       # environment variables file
+
+# Instantiate logging in accordance with config file
+fileConfig('../utils/logging_config.ini')
+logger = logging.getLogger('discord')
+
+# Explicit start of the bot runtime
+logger.info("Started Discord bot")
+
+try:
+    # Check if it is PROD or TEST environment
+    environment = get_env('ENV', __file__)
+    logger.info("Running on environment: {}".format(environment))
+
+    # Get TOKEN environment variable
+    token = get_env('TOKEN', __file__)
+    logger.info("Got Discord token")
+except Exception as e:
+    logger.exception("Could not get environment variables: {}".format(str(vars(e))))
+
+try:
+    # Instatiate Discord client
+    client = discord.Client()
+    logger.info("Instantiated Discord client")
+except Exception as e:
+    logger.exception("Error while instantiating Discord client: {}".format(str(vars(e))))
+
+@client.event
+async def on_message(message):
+    # If running in test environment, do not reply to any calls
+    if environment == 'TEST':
+        return
+
+    # Do not reply to comments from these users, including itself (client.user)
+    blocked_users = [ client.user ]
+
+    # Bot does not reply to itself and only when mentioned
+    if client.user.mentioned_in(message) and message.author not in blocked_users:
+        logger.info("Replied to message of user '{}' in server '{}' / channel '{}'".format(message.author, message.server, message.channel))
+        msg = get_random_quote().format(message)
+        await client.send_message(message.channel, msg)
+        
+@client.event
+async def on_server_join(server):
+    logger.info("Bot added to server '{}'".format(server.name))
+    logger.info("Bot currently running on {} server(s)".format(len(client.servers)))
+    
+@client.event
+async def on_ready():
+    logger.info("Logged in as '{}', client ID '{}'". format(client.user.name, client.user.id))
+    logger.info("Bot currently running on {} server(s)".format(len(client.servers)))
+
+if __name__ == '__main__':
+    try:
+        # Run Discord bot
+        client.run(token)
+        logger.info("Started Discord client")
+    except Exception as e:
+        logger.exception("Error while running Discord client: {}".format(str(vars(e))))
